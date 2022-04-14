b359c5c7c38cc428f5f322286b12d1adfd8f37e4
Thaneat Saithong <thaneat.s@lmwn.com>
2022-04-14 16:38:01 +0700

    add simple grpc server

---
+++ grpc-server/src/main/java/com/nest/grpc/GrpcService.java
@@ -0,0 +1,15 @@
+package com.nest.grpc;
+
+import io.grpc.stub.StreamObserver;
+import org.lognet.springboot.grpc.GRpcService;
+
+@GRpcService
+public class GrpcService extends HelloServiceGrpc.HelloServiceImplBase {
+    @Override
+    public void hello(HelloWorld.Hello req, StreamObserver<HelloWorld.Greeting> responseObserver) {
+        String greeting = String.format("Hello, %s %s.", req.getFirstName(), req.getLastName());
+        HelloWorld.Greeting res = HelloWorld.Greeting.newBuilder().setGreeting(greeting).build();
+        responseObserver.onNext(res);
+        responseObserver.onCompleted();
+    }
+}
